e40aabd49f4e344a9863206374c82dec5bd7fb4e
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-07-31 19:50:06 -0700

    set up theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { createTheme } from '@mui/material/styles'
+import { useMemo } from 'react'
+import { themeSettings } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
 function App() {
-  return <div className='app'></div>
+  const theme = useMemo(() => createTheme(themeSettings), []) // make sure to pass empty array as second argument, otherwise it will create new theme on every render
+
+  return (
+    <div className='app'>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        Hi
+      </ThemeProvider>
+    </div>
+  )
 }
 
 export default App
